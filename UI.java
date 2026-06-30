36e437830e7a3b968e725ac69ab0e94a4bff01e4
npraveen18 <nihalpraveen18@gmail.com>
2026-06-30 13:17:34 -0500

    Some visual upgrades

--- UI.java
+++ UI.java
@@ -12,6 +12,15 @@ public class UI{
         
         ArrayList<JButton> buttons = new ArrayList<>();
 
+        
+
+        Color mauve = new Color(0xdabfff);
+        Color softPeriwinkle = new Color(0x907ad6);
+        Color dustyGrape = new Color(0x4f518c);
+        Color spaceIndigo = new Color(0x2c2a4a);
+
+        colors[0] = mauve;
+        colors[1] = softPeriwinkle;
 
         int screenWidth = (int)(Toolkit.getDefaultToolkit().getScreenSize().width);
         int screenHeight = (int)(Toolkit.getDefaultToolkit().getScreenSize().height);
@@ -20,37 +29,42 @@ public class UI{
         JFrame frame = new JFrame("Student Planner App");
         frame.setSize(400, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(null);
+        frame.setForeground(spaceIndigo);
+        //frame.setLayout(null);
         //frame
 
-        ClassRoom example = new ClassRoom("Math", 101);
+        JPanel backgroundPanel = new JPanel();
+        backgroundPanel.setBackground(spaceIndigo);
+        frame.setContentPane(backgroundPanel);
+        frame.setLayout(null);
+        
 
         //input
         JTextField input = new JTextField();
-        input.setBounds(100,100,100,100);
+        input.setBounds(10,410,200,50);
         //input
 
         //container for labels
         JPanel container = new JPanel();
-        container.setBounds(0,0,screenWidth,100);
-        container.setBackground(Color.red);
-        
+        container.setBounds(0,0,400,400);
+        container.setBackground(spaceIndigo);
+        container.setLayout(new GridLayout(0, 1, 0, 2));
 
         JButton addClass = new JButton();
-        addClass.setBounds(200,100,100,100);
+        addClass.setBounds(220,410,160,50);
         addClass.setText("Add Class");
-        addClass.setBackground(Color.yellow);
+        addClass.setBackground(dustyGrape);
         addClass.addActionListener(e -> {
             if (!input.getText().equals("")){
                 ClassRoom addition = new ClassRoom(input.getText(), 101);
                 classes.add(addition);
                 alternator = 1 - alternator;
                 JButton but = new JButton(addition.getName());
-                but.setBounds(0,0,50,50);
+                but.setBounds(0,0,50, 50);
                 but.setBackground(colors[alternator]);
                 but.addActionListener(s -> {
                     JFrame window = new JFrame(addition.getName());
-                    window.setSize(200,200);
+                    window.setSize(350,400);
                     window.setLayout(null);
                     window.setLocationRelativeTo(null);
                     window.setVisible(true);
@@ -75,22 +89,28 @@ public class UI{
                     }
 
                     JTextField secondInput = new JTextField();
-                    secondInput.setBounds(50, 50, 100, 100);
+                    secondInput.setBounds(5, 310, 165, 50);
                     window.add(secondInput);
 
                     JButton assignmentAdder = new JButton("Add Assignment");
-                    assignmentAdder.setBounds(150,50,100,100);
+                    assignmentAdder.setBounds(170,310,160,50);
                     window.add(assignmentAdder);
 
                     assignmentAdder.addActionListener(a -> {
-                        JLabel lLabel = new JLabel(secondInput.getText());
-                        lLabel.setBounds(0,0,100,100);
-                        cont.add(lLabel);
+                        if (!secondInput.getText().equals("")){
+                            JLabel lLabel = new JLabel(secondInput.getText());
+                            Assignment k = new Assignment(secondInput.getText());
+                            addition.addAssignment(k);
+                            lLabel.setBounds(0,0,100,100);
+                            cont.add(lLabel);
+                            cont.revalidate();
+                            cont.repaint();
+                        }
                     });
 
 
-                    cont.revalidate();
-                    cont.repaint();
+                    window.revalidate();
+                    window.repaint();
                 });
                 container.add(but);
                 buttons.add(but);
